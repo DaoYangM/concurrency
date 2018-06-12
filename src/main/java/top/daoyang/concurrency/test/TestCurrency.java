426cc6123c5201443ebd7926ce8193f51eed920c
DaoYangM <498721777@qq.com>
2018-06-12 21:54:56 +0800

    TestCurrency.class

---
+++ src/main/java/top/daoyang/concurrency/test/TestCurrency.java
@@ -0,0 +1,44 @@
+package top.daoyang.concurrency.test;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Semaphore;
+
+@Slf4j
+public class TestCurrency {
+
+    private static int clientTotal = 5000;
+
+    private static int threadTotal = 50;
+
+    private static int count = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
+
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                add();
+                countDownLatch.countDown();
+                semaphore.release();
+            });
+        }
+
+        countDownLatch.await();
+        log.info("count: {}",  count);
+    }
+
+    public static void add() {
+        count++;
+    }
+}
